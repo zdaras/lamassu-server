ff0642d6712c49dbe5e4fcddad7897ae687629f6
Josh Harvey <josh@lamassu.is>
2018-05-26 14:09:47 +0300

    WIP

---
+++ dev/strike.js
@@ -0,0 +1,19 @@
+const strike = require('../lib/plugins/wallet/strike/strike')
+const BN = require('../lib/bn')
+
+const account = {token: 'sk_HddXz3grg1KVCjJLjtUdbFQnFGaFM'}
+strike.newAddress(account, {cryptoCode: 'BTC', cryptoAtoms: BN(10000)})
+  .then(r => {
+    console.log(r)
+
+    const toAddress = r
+    const requested = null
+    const cryptoCode = 'BTC'
+
+    setInterval(() => {
+      strike.getStatus(account, toAddress, requested, cryptoCode)
+        .then(console.log)
+        .catch(r => console.log(r.message))
+    }, 2000)
+  })
+  .catch(console.log)
